90558148abe3b9a1042c5fab332e79d75c8c483d
Austin <austinobejo@gmail.com>
2020-02-17 01:45:47 -0500

    Implemented ssl cert

--- deploy/create.sh
+++ deploy/create.sh
@@ -1,2 +1,2 @@
 docker build -t personal-website-image .
-docker run -dit --name personal-website-running -p 8080:80 personal-website-image
+docker run -dit --name personal-website-running -p 80:80 -p 443:443 personal-website-image
